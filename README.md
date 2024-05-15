154eb899fc76669e5208744f5d64220cfec9d735
Bill Nixon <bnixon67@gmail.com>
2024-05-14 22:13:43 -0500

    cleanup; added LICENSE

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hello-c
-Hello World for C
+Hello World program in C
+
+Prints a simple message, along with the number and value of all the command
+line arguments.
--- hello.c
+++ hello.c
@@ -1,12 +1,15 @@
+// Copyright 2024 Bill Nixon. All rights reserved.
+// Use of this source code is governed by the license found in the LICENSE file.
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	printf("hello");
 
 	printf("argc = %d\n", argc);
 	for (int n = 0; n < argc; n++) {
-		printf("argv[%d] = %s\n", n, argv[n]);
+		printf("argv[%d] = \"%s\"\n", n, argv[n]);
 	}
 
-	return(0);
+	return 0;
 }
